948d3e65ef833e0423e0e8781a07e67b1bd88c80
frstock <baudelaire2>
2016-04-21 16:39:47 +0200

    Game routines (logic & draw)

--- scripts/app.js
+++ scripts/app.js
@@ -1,40 +1,60 @@
 var app = (function($, game) {
-    var
-        draw = function() {
-            drawTarget();
-            drawPlaques();
-            drawOperators();
-            drawOperation();
-        },
-        drawTarget = function() {
-            var
-                map = game.map,
-                $target = $("#target");
+	var
+		draw = function() {
+			drawTarget();
+			drawPlaques();
+			drawOperators();
+			drawOperation();
+		},
+		drawTarget = function() {
+			var
+				map = game.map,
+				$target = $("#target");
 
-            $target.html(map.target);
-        },
-        drawPlaques = function() {
-            var
-                map = game.map,
-                plaques = [];
+			$target.html(map.target);
+		},
+		drawPlaques = function() {
+			var
+				map = game.map,
+				plaques = [];
 
-            $("#plaques").empty();
-            $.each(map.plaques, function(index, value) {
-                var item = "<li>" + value + "</li>";
+			$("#plaques").empty();
+			$.each(map.plaques, function(index, value) {
+				var $li = $("<li />").html(value);
+
+				if (game.isOperandSelectable()) {
+					$li.click(function() {
+						game.selectPlaque(index);
+					});
+				} else {
+                    $li.addClass("disabled");
+                    $li.attr("disabled", true);
+                }
+
+				$li.appendTo("#plaques");
+			});
+
+		},
+		drawOperators = function() {
+			$.each(game.operators, function(index, value) {
+				var                    $li = $("<li />").html(value);
                 
-                console.log(item);
-                
-                $("#plaques").append(item);
-            });
+                if (game.isOperandSelectable()) {
+                    $li.addClass("disabled");
+                    $li.attr("disabled", true);
+				} else {
+					$li.click(function() {
+						game.selectOperator(value);
+					});
+                }
 
-        },
-        drawOperators = function() {
-            $.each(game.operators, function(index, value) {
-                console.log(index + "/" + value);
-            });
-        },
-        drawOperation = function() {};
+				$li.appendTo("#operators");
+			});
+		},
+		drawOperation = function() {
 
-    game.init();
-    draw();
+		};
+
+	game.init();
+	draw();
 })(jQuery, game);
--- scripts/game.js
+++ scripts/game.js
@@ -1,88 +1,115 @@
 var game = (function($) {
-    var
-        map = {
-            target: 0,
-            plaques: [],
-            currentOperation: {}
-        },
-        availablePlaques = [
-            1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-            1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-            25, 75, 100
-        ],
-        getBlankOperation = function() {
-            return {
-                leftOperand: 0,
-                operator: {},
-                rightOperand: 0
-            };
-        },
-        isOperandSelectable = function() {
-            var operation = map.currentOperation;
-            
-            return operation.leftOperand === 0 || operation.operator !== {};
-        },
+	var
+		map = {
+			target: 0,
+			plaques: [],
+			currentOperation: {}
+		},
+		availablePlaques = [
+			1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+			1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+			25, 75, 100
+		],
+		getBlankOperation = function() {
+			return {
+				leftOperand: 0,
+				operator: "",
+				rightOperand: 0
+			};
+		},
 
-        addition = function(leftOperand, rightOperand) {
-            return leftOperand + rightOperand;
-        },
-        substraction = function(leftOperand, rightOperand) {
-            return leftOperand - rightOperand < 0 ? 0 : leftOperand - rightOperand;
-        },
-        multiplication = function(leftOperand, rightOperand) {
-            return leftOperand * rightOperand;
-        },
-        division = function(leftOperand, rightOperand) {
-            return leftOperand / rightOperand !== Math.floor(leftOperand / rightOperand) ?
-                0 :
-                leftOperand / rightOperand;
-        },
-        operators = {
-            "+": addition,
-            "-": substraction,
-            "*": multiplication,
-            ":": division
-        },
+		addition = function(leftOperand, rightOperand) {
+			return leftOperand + rightOperand;
+		},
+		substraction = function(leftOperand, rightOperand) {
+			return leftOperand - rightOperand < 0 ? 0 : leftOperand - rightOperand;
+		},
+		multiplication = function(leftOperand, rightOperand) {
+			return leftOperand * rightOperand;
+		},
+		division = function(leftOperand, rightOperand) {
+			return leftOperand / rightOperand !== Math.floor(leftOperand / rightOperand) ?
+				0 :
+				leftOperand / rightOperand;
+		},
+		operators = {
+			"+": addition,
+			"-": substraction,
+			"*": multiplication,
+			":": division
+		},
 
-        // initialization
-        init = function() {
-            initTarget();
-            initPlaques();
-            initOperation();
-        },
-        initTarget = function() {
-            map.target = getRandomInt(100, 999);
+		// initialization
+		init = function() {
+			initTarget();
+			initPlaques();
+			initOperation();
+		},
+		initTarget = function() {
+			map.target = getRandomInt(100, 999);
 
-            console.log("Target initialized: " + map.target);
-        },
-        initPlaques = function() {
-            var
-                temp = availablePlaques.slice(),
-                count;
+			console.log("Target initialized: " + map.target);
+		},
+		initPlaques = function() {
+			var
+				temp = availablePlaques.slice(),
+				count;
 
-            map.plaques = [];
+			map.plaques = [];
 
-            for (count = 0; count < 6; ++count) {
-                var
-                    index = getRandomInt(0, temp.length - 1),
-                    number = temp.splice(index, 1)[0];
+			for (count = 0; count < 6; ++count) {
+				var
+					index = getRandomInt(0, temp.length - 1),
+					number = temp.splice(index, 1)[0];
 
-                map.plaques.push(number);
-            }
+				map.plaques.push(number);
+			}
 
-            console.log("Plaques initialized: " + map.plaques);
-        },
-        initOperation = function() {
-            map.currentOperation = getBlankOperation();
-            console.log("Operation initialized.");
-        },
-        getRandomInt = function(minimum, maximum) {
-            return Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
-        };
+			console.log("Plaques initialized: " + map.plaques);
+		},
+		initOperation = function() {
+			map.currentOperation = getBlankOperation();
+			console.log("Operation initialized.");
+		},
+		getRandomInt = function(minimum, maximum) {
+			return Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+		}
 
-    return {
-        map: map,
-        operators: operators,
-        init: init
-    };
+	// Game routines
+	selectOperand = function(index) {
+			if (!isOperandSelectable()) {
+				throw Error("Illegal operation: operator is already selected.");
+			}
+
+			var
+				operation = game.map.currentOperation,
+				value = game.map.plaques.slice(index, 1)[0];
+
+			if (operation.leftOperand === 0) {
+				operation.leftOperand = value;
+			} else if (operation.rightOperand === 0) {
+				operation.rightOperand = value;
+			}
+		},
+		selectOperator = function(operator) {
+			if (!isOperandSelectable()) {
+				throw Error("Illegal operation: operator is already selected.");
+			}
+
+			game.currentOperation.operator = operator;
+		},
+		isOperandSelectable = function() {
+			var operation = map.currentOperation;
+
+			return operation.leftOperand === 0 || operation.operator !== "";
+		};
+
+	return {
+		map: map,
+		operators: operators,
+		init: init,
+		selectOperand: selectOperand,
+		selectOperator: selectOperator,
+		isOperandSelectable: isOperandSelectable
+	};
 })(jQuery);
